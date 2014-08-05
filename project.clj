3ca3239b2aa6ff39d8f04db22f86beec01720e57
Alan Thompson <cloojure@gmail.com>
2014-08-05 12:14:03 -0700

    misc

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cooljure "0.1.0-SNAPSHOT"
+(defproject cooljure "0.1.1-SNAPSHOT"
   :description "Cooljure:  Cool stuff you wish was in Clojure"
   :url "http://github.com/cloojure/cooljure"
   :license {:name "Eclipse Public License"
