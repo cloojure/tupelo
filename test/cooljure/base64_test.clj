4e40658d4cd59b8e38e51473e09f54f4219c7e70
Alan Thompson <cloojure@gmail.com>
2014-11-09 01:23:16 -0800

    misc

--- test/cooljure/base64_test.clj
+++ test/cooljure/base64_test.clj
@@ -11,6 +11,7 @@
             [clojure.data.generators                  :as gen]
             [cooljure.base64                          :as b64]
             [cooljure.types                           :as types] 
+            [cooljure.misc                            :as misc] 
             [schema.core                              :as s] )
   (:use cooljure.core
         clojure.test)
@@ -26,15 +27,25 @@
 
 (def b64-data-chars
   "A set of Base64 data characters a-z, A-A, 0-9 (not including padding characters like =)"
-  (let [
-  ]
-  ))
+  (into #{} (flatten [ (misc/char-seq  \a \z)
+                       (misc/char-seq  \A \Z)
+                       (misc/char-seq  \0 \9) ] )))
+
+(def base64-chars
+  "A set of chars for traditional Base64 symbols, with padding chars [ + \\ = ]"
+  (into b64-data-chars [\+ \\ \=] ))
+(spy-expr base64-chars)
+
+(def y64-chars
+  "A set of chars for the URL-safe Yahoo variant of Base64 symbols, with padding chars [ . _ - ]
+   (see YUI library)"
+  (into b64-data-chars [\. \_ \-] ))
+(spy-expr y64-chars)
 
 (def chars-seq
   "A seq of 1-char strings of all printable characters from space (32) to tilde (126)"
-  (mapv str 
-    (mapv char 
-      (range 32 (inc 126)))))
+  (mapv str (misc/char-seq \space \~)))
+; (spy-expr chars-seq)
 
 (defn seq-of-bytes
   "Return a list or vec or bytes"
@@ -42,21 +53,34 @@
   (gen/one-of   (gen/list gen/byte)
                 (gen/vec  gen/byte)))
 
-; (defn bytes-round-trip
-;   "Transform a seq of bytes to a base64 string and back."
-;   [bytes-in]
-;   (let [string-b64      (b64/encode-bytes->str      bytes-in)
-;         bytes-out       (b64/decode-str->bytes      string-b64) ]
-;     (assert (every? 
-; 
+(defn b64-round-trip-bytes
+  "Transform a seq of bytes to a base64 string and back."
+  [bytes-in]
+  (let [string-b64      (b64/encode-bytes->str      bytes-in)
+        bytes-out       (b64/decode-str->bytes      string-b64) ]
+    (spy-expr string-b64)
+    (assert (every? base64-chars (seq string-b64)))
+    bytes-out))
+
 ; (tg/defspec bytes-t
 
-#_(deftest sample-t
+(defn seq->str
+  "pr a seq to stdout"
+  [seq-in]
+  (with-out-str
+    (doseq [it seq-in]
+      (print \space)
+      (pr it))))
+
+(deftest sample-t
   (testing "basic usage"
     (println \newline "#1")
-    (println 
-      (b64/encode-bytes->str 
-        (byte-array (map #(.byteValue %) (range 0 400 50)))))))
+    (let [step 50
+          data  (byte-array (mapv #(.byteValue %) (range 0 400 step)))
+          _ (spy-expr (seq->str data))
+          result (b64-round-trip-bytes data)
+          _ (spy-expr (seq->str result)) ]
+      (assert (= (seq data) (seq result))))))
 
 #_(defn -main []
     (println \newline "#9")
