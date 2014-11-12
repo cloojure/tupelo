839f7e732c7135c5324e9a65ee3915ba1245e75a
Alan Thompson <cloojure@gmail.com>
2014-11-11 22:11:30 -0800

    misc

--- src/cooljure/types.clj
+++ src/cooljure/types.clj
@@ -9,16 +9,64 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
-; An instance of the java.lang.Class<Byte[]>.  The representation of a byte array's class.
-(defonce Class-byte-array
-  (Class/forName "[B"))
+(def ^:const UTF-8-Charset-Name "UTF-8")
+
+; An instance of the java.lang.Class<XXXX[]> (e.g. java.lang.Class<Byte[]>). 
+(defonce ^:private Class-byte-array         (.getClass (byte-array      0)))
+(defonce ^:private Class-boolean-array      (.getClass (boolean-array   0)))
+(defonce ^:private Class-char-array         (.getClass (char-array      0)))
+(defonce ^:private Class-double-array       (.getClass (double-array    0)))
+(defonce ^:private Class-float-array        (.getClass (float-array     0)))
+(defonce ^:private Class-int-array          (.getClass (int-array       0)))
+(defonce ^:private Class-long-array         (.getClass (long-array      0)))
+(defonce ^:private Class-object-array       (.getClass (object-array    0)))
+(defonce ^:private Class-short-array        (.getClass (short-array     0)))
 
 (defn byte-array?
   "Returns true is the arg is a byte array, else false."
   [arg]
   (= Class-byte-array (.getClass arg)))
 
-(def ^:const UTF-8-Charset-Name "UTF-8")
+(defn boolean-array?
+  "Returns true is the arg is a boolean array, else false."
+  [arg]
+  (= Class-boolean-array (.getClass arg)))
+
+(defn char-array?
+  "Returns true is the arg is a char array, else false."
+  [arg]
+  (= Class-char-array (.getClass arg)))
+
+(defn double-array?
+  "Returns true is the arg is a double array, else false."
+  [arg]
+  (= Class-double-array (.getClass arg)))
+
+(defn float-array?
+  "Returns true is the arg is a float array, else false."
+  [arg]
+  (= Class-float-array (.getClass arg)))
+
+(defn int-array?
+  "Returns true is the arg is a int array, else false."
+  [arg]
+  (= Class-int-array (.getClass arg)))
+
+(defn long-array?
+  "Returns true is the arg is a long array, else false."
+  [arg]
+  (= Class-long-array (.getClass arg)))
+
+(defn object-array?
+  "Returns true is the arg is a object array, else false."
+  [arg]
+  (= Class-object-array (.getClass arg)))
+
+(defn short-array?
+  "Returns true is the arg is a short array, else false."
+  [arg]
+  (= Class-short-array (.getClass arg)))
+
 
 (defn str->bytes
   "Converts a String to a byte array using the UTF-8 Charset"
