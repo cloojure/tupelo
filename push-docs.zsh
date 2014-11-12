b64a125bf6c9d07b089ddd6654b9e8941ae1d76b
Alan Thompson <cloojure@gmail.com>
2014-11-11 21:38:52 -0800

    Update docs for cooljure

--- push-docs.zsh
+++ push-docs.zsh
@@ -1,7 +1,13 @@
 #!/bin/zsh -v
-pushd doc
-git checkout gh-pages   # To be sure you're on the right branch
-git add .
-git commit -am "new documentation push."
-git push -u origin gh-pages
+
+ghPagesDir="~/gh-pages"
+projName="cooljure"
+docsDir=$(pwd)/doc
+
+pushd $ghPagesDir
+  rm -rf             $projName
+  cp -pr   $docsDir  $projName
+  git add            $projName
+  git commit --all -m"Update docs for $projName"
+  git push
 popd
