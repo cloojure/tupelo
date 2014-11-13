a454e17f40e9706414d036bc87803d775757d589
Alan Thompson <cloojure@gmail.com>
2014-11-12 17:56:07 -0800

    misc

--- test/cooljure/parse_test.clj
+++ test/cooljure/parse_test.clj
@@ -7,121 +7,107 @@
 
 (ns cooljure.parse-test
   (:require [cooljure.parse             :as cool]
-            [clojure.test               :refer :all] )
+            [clojure.test               :refer :all] 
+            [cooljure.core              :refer [rel=]] )
   (:import   [java.lang.Math] ))
-
-(defn error-ratio
-  "Compute the error ratio for two floating-point values."
-  [ val1 val2 ]
-  (let [  dbl1         (double val1)
-          dbl2         (double val2)
-          abs-delta    (Math/abs (- dbl1 dbl2))
-          max-abs-val  (Math/max (double (Math/abs dbl1))
-                                 (double (Math/abs dbl2)) )
-          error-ratio  (/ abs-delta max-abs-val) ]
-    error-ratio 
-  ))
-; AWTAWT TODO: handle [0  0] case
 
 (deftest parse-byte
   (testing "basic"
-    (is (= 15                              (cool/parse-byte "15")))
-    (is (= -5                              (cool/parse-byte "-5")))
-    (is (thrown? NumberFormatException     (cool/parse-byte "999")))
-    (is (thrown? NumberFormatException     (cool/parse-byte " "))) )
+    (is (= 15                               (cool/parse-byte "15")))
+    (is (= -5                               (cool/parse-byte "-5")))
+    (is (thrown? NumberFormatException      (cool/parse-byte "999")))
+    (is (thrown? NumberFormatException      (cool/parse-byte " "))) )
   (testing "with :or"
-    (is (= 15      (cool/parse-byte "15"                           :or nil )))
-    (is (= -5      (cool/parse-byte "-5"                           :or nil )))
-    (is (= nil     (cool/parse-byte "999"                          :or nil )))
-    (is (= nil     (cool/parse-byte ""                             :or nil )))
-    (is (= 0       (cool/parse-byte "xyz"                          :or 0   ))) ))
+    (is (= 15                               (cool/parse-byte "15"             :or nil )))
+    (is (= -5                               (cool/parse-byte "-5"             :or nil )))
+    (is (= nil                              (cool/parse-byte "999"            :or nil )))
+    (is (= nil                              (cool/parse-byte ""               :or nil )))
+    (is (= 0                                (cool/parse-byte "xyz"            :or 0   ))) ))
 
 (deftest parse-short
   (testing "basic"
-    (is (= 15                              (cool/parse-short "15")))
-    (is (= -5                              (cool/parse-short "-5")))
-    (is (= 999                             (cool/parse-short "999")))
-    (is (thrown? NumberFormatException     (cool/parse-short "99999")))
-    (is (thrown? NumberFormatException     (cool/parse-short" "))) )
+    (is (= 15                               (cool/parse-short "15")))
+    (is (= -5                               (cool/parse-short "-5")))
+    (is (= 999                              (cool/parse-short "999")))
+    (is (thrown? NumberFormatException      (cool/parse-short "99999")))
+    (is (thrown? NumberFormatException      (cool/parse-short" "))) )
   (testing "with :or"
-    (is (= 15      (cool/parse-short "15"                          :or nil )))
-    (is (= -5      (cool/parse-short "-5"                          :or nil )))
-    (is (= 999     (cool/parse-short "999"                         :or nil )))
-    (is (= nil     (cool/parse-short "99999"                       :or nil )))
-    (is (= nil     (cool/parse-short ""                            :or nil )))
-    (is (= 0       (cool/parse-short "xyz"                         :or 0   ))) ))
+    (is (= 15                               (cool/parse-short "15"            :or nil )))
+    (is (= -5                               (cool/parse-short "-5"            :or nil )))
+    (is (= 999                              (cool/parse-short "999"           :or nil )))
+    (is (= nil                              (cool/parse-short "99999"         :or nil )))
+    (is (= nil                              (cool/parse-short ""              :or nil )))
+    (is (= 0                                (cool/parse-short "xyz"           :or 0   ))) ))
 
 (deftest parse-int
   (testing "basic"
-    (is (= 15                              (cool/parse-int "15")))
-    (is (= -5                              (cool/parse-int "-5")))
-    (is (= 99999                           (cool/parse-int "99999")))
-    (is (thrown? NumberFormatException     (cool/parse-int "9876543210")))
-    (is (thrown? NumberFormatException     (cool/parse-int ""))) )
+    (is (= 15                               (cool/parse-int "15")))
+    (is (= -5                               (cool/parse-int "-5")))
+    (is (= 99999                            (cool/parse-int "99999")))
+    (is (thrown? NumberFormatException      (cool/parse-int "9876543210")))
+    (is (thrown? NumberFormatException      (cool/parse-int ""))) )
   (testing "with :or"
-    (is (= 15      (cool/parse-int "15"                            :or nil )))
-    (is (= -5      (cool/parse-int "-5"                            :or nil )))
-    (is (= 99999   (cool/parse-int "99999"                         :or nil )))
-    (is (= nil     (cool/parse-int "9876543210"                    :or nil )))
-    (is (= nil     (cool/parse-int ""                              :or nil )))
-    (is (= 0       (cool/parse-int "xyz"                           :or 0   ))) ))
+    (is (= 15                               (cool/parse-int "15"              :or nil )))
+    (is (= -5                               (cool/parse-int "-5"              :or nil )))
+    (is (= 99999                            (cool/parse-int "99999"           :or nil )))
+    (is (= nil                              (cool/parse-int "9876543210"      :or nil )))
+    (is (= nil                              (cool/parse-int ""                :or nil )))
+    (is (= 0                                (cool/parse-int "xyz"             :or 0   ))) ))
 
 (deftest parse-long
   (testing "basic"
-    (is (= 15                              (cool/parse-long "15")))
-    (is (= -5                              (cool/parse-long "-5")))
-    (is (= 99999                           (cool/parse-long "99999")))
-    (is (= 9876543210                      (cool/parse-long "9876543210")))
-    (is (thrown? NumberFormatException     (cool/parse-long "98765432109876543210")))
-    (is (thrown? NumberFormatException     (cool/parse-long ""))) )
+    (is (= 15                               (cool/parse-long "15")))
+    (is (= -5                               (cool/parse-long "-5")))
+    (is (= 99999                            (cool/parse-long "99999")))
+    (is (= 9876543210                       (cool/parse-long "9876543210")))
+    (is (thrown? NumberFormatException      (cool/parse-long "98765432109876543210")))
+    (is (thrown? NumberFormatException      (cool/parse-long ""))) )
   (testing "with :or"
-    (is (= 15           (cool/parse-long "15"                            :or nil )))
-    (is (= -5           (cool/parse-long "-5"                            :or nil )))
-    (is (= 99999        (cool/parse-long "99999"                         :or nil )))
-    (is (= 9876543210   (cool/parse-long "9876543210"                    :or nil )))
-    (is (= nil          (cool/parse-long "98765432109876543210"          :or nil )))
-    (is (= nil          (cool/parse-long ""                              :or nil ))) 
-    (is (= 0            (cool/parse-long "xyz"                           :or 0   ))) ))
+    (is (= 15                               (cool/parse-long "15"                     :or nil )))
+    (is (= -5                               (cool/parse-long "-5"                     :or nil )))
+    (is (= 99999                            (cool/parse-long "99999"                  :or nil )))
+    (is (= 9876543210                       (cool/parse-long "9876543210"             :or nil )))
+    (is (= nil                              (cool/parse-long "98765432109876543210"   :or nil )))
+    (is (= nil                              (cool/parse-long ""                       :or nil ))) 
+    (is (= 0                                (cool/parse-long "xyz"                    :or 0   ))) ))
 
 (deftest parse-float
   (testing "basic"
-    (is (= 15.0                            (cool/parse-float "15")))
-    (is (= -5.0                            (cool/parse-float "-5")))
-    (is (thrown? NumberFormatException     (cool/parse-float "")))
-    (is (thrown? NumberFormatException     (cool/parse-float "xyz")))
-    (is (= 0.5                             (cool/parse-float "0.5")))
-    (is (> 1e-7 (error-ratio               (cool/parse-float "0.1") 0.1 )))
-    (is (> 1e-7 (error-ratio               (cool/parse-float "3.141592654") 3.141592654 ))) )
+    (is (= 15.0                             (cool/parse-float "15")))
+    (is (= -5.0                             (cool/parse-float "-5")))
+    (is (= 0.5                              (cool/parse-float "0.5")))
+    (is (rel=  0.1                          (cool/parse-float "0.1")            :digits 7))
+    (is (rel=  3.141592654                  (cool/parse-float "3.141592654")    :digits 7))
+    (is (thrown? NumberFormatException      (cool/parse-float "")))
+    (is (thrown? NumberFormatException      (cool/parse-float "xyz")))
 
   (testing "with :or"
-    (is (= 15.0                     (cool/parse-float "15"               :or nil )))
-    (is (= -5.0                     (cool/parse-float "-5"               :or nil )))
-    (is (= nil                      (cool/parse-float ""                 :or nil )))
-    (is (= 0                        (cool/parse-float "xyz"              :or 0   )))
-    (is (= 0.5                      (cool/parse-float "0.5"              :or nil )))
-    (is (> 1e-7 (error-ratio        (cool/parse-float "0.1"              :or 0) 
-                                                      0.1 )))
-    (is (> 1e-7 (error-ratio        (cool/parse-float "3.141592654"      :or 0) 
-                                                      3.141592654 ))) 
+    (is (= 15.0                             (cool/parse-float "15"               :or nil )))
+    (is (= -5.0                             (cool/parse-float "-5"               :or nil )))
+    (is (= nil                              (cool/parse-float ""                 :or nil )))
+    (is (= 0                                (cool/parse-float "xyz"              :or 0   )))
+    (is (= 0.5                              (cool/parse-float "0.5"              :or nil )))
+    (is (rel=  (/ 1 10)                     (cool/parse-float "0.1"              :or 0) :digits 7))
+    (is (rel=  3.141592654                  (cool/parse-float "3.141592654"      :or 0) :digits 7)))
   ))
 
 (deftest parse-double
   (testing "basic"
-    (is (= 15.0                            (cool/parse-double "15")))
-    (is (= -5.0                            (cool/parse-double "-5")))
-    (is (thrown? NumberFormatException     (cool/parse-double "")))
-    (is (thrown? NumberFormatException     (cool/parse-double "xyz")))
-    (is (= 0.5                             (cool/parse-double "0.5")))
-    (is (> 1e-9 (error-ratio               (cool/parse-double "0.1") (double (/ 1 10) ))))
-    (is (> 1e-9 (error-ratio               (cool/parse-double "3.141592654") Math/PI   ))) )
+    (is (= 15.0                             (cool/parse-double "15")))
+    (is (= -5.0                             (cool/parse-double "-5")))
+    (is (thrown? NumberFormatException      (cool/parse-double "")))
+    (is (thrown? NumberFormatException      (cool/parse-double "xyz")))
+    (is (= 0.5                              (cool/parse-double "0.5")))
+    (is (rel=  (double (/ 1 10) )           (cool/parse-double "0.1")           :digits 9))
+    (is (rel=  Math/PI                      (cool/parse-double "3.141592654")   :digits 9))) 
 
   (testing "with :or"
-    (is (= 15.0                     (cool/parse-double "15"              :or nil )))
-    (is (= -5.0                     (cool/parse-double "-5"              :or nil )))
-    (is (= nil                      (cool/parse-double ""                :or nil )))
-    (is (= 0                        (cool/parse-double "xyz"             :or 0   )))
-    (is (= 0.5                      (cool/parse-double "0.5"             :or nil )))
-    (is (> 1e-9 (error-ratio        (cool/parse-double "0.1" :or 0) (double (/ 1 10) ))))
-    (is (> 1e-9 (error-ratio        (cool/parse-double "3.141592654" :or 0) Math/PI   ))) 
+    (is (= 15.0                             (cool/parse-double "15"          :or nil )))
+    (is (= -5.0                             (cool/parse-double "-5"          :or nil )))
+    (is (= nil                              (cool/parse-double ""            :or nil )))
+    (is (= 0                                (cool/parse-double "xyz"         :or 0   )))
+    (is (= 0.5                              (cool/parse-double "0.5"         :or nil )))
+    (is (rel= (/ 1 10)                      (cool/parse-double "0.1"         :or 0)     :digits 9))
+    (is (rel= Math/PI                       (cool/parse-double "3.141592654" :or 0)     :digits 9))
   ))
 
