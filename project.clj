a96c1dcec02f4dedb644a93e0affbdf7dabfe7a2
Alan Thompson <cloojure@gmail.com>
2015-10-19 11:51:48 -0700

    misc

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/cloojure/tupelo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [ [org.clojure/clojure              "1.6.0"]
+  :dependencies [ [org.clojure/clojure              "1.7.0"]
                   [org.clojure/core.incubator       "0.1.3"]
                   [org.clojure/test.check           "0.5.9"]
                   [org.clojure/core.match           "0.3.0-alpha4"]
