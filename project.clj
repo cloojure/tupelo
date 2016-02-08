640c03eed9e003c26b2bfa14eeaaa4483dcae7cd
Alan Thompson <cloojure@gmail.com>
2016-02-07 16:53:19 -0800

    version 0.1.61

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tupelo "0.1.60"
+(defproject tupelo "0.1.61"
   :description "Tupelo:  Making Clojure Even Sweeter"
   :url "http://github.com/cloojure/tupelo"
   :license {:name "Eclipse Public License"
@@ -7,16 +7,11 @@
                   [org.clojure/core.incubator       "0.1.3"]
                   [org.clojure/test.check           "0.5.9"]
                   [org.clojure/core.match           "0.3.0-alpha4"]
-                  [org.clojure/java.jdbc            "0.4.2"] 
-                  [java-jdbc/dsl                    "0.1.3"]
-                  [org.postgresql/postgresql        "9.4-1206-jdbc42"]
-                  [com.mchange/c3p0                 "0.9.5.2"]
                   [clojure-csv/clojure-csv          "2.0.2"]
                   [clj-time                         "0.11.0"]
                   [criterium                        "0.4.3"]
                   [cheshire                         "5.5.0"]
                   [prismatic/schema                 "1.0.4"]
-                  [honeysql                         "0.6.2"]
                 ]
   :plugins  [ [codox "0.9.1"] ]
   :codox {:src-dir-uri "http://github.com/cloojure/tupelo/blob/master/"
