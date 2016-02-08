de969c30c0983062694eb7a6f3ca3dd3e4982ba0
Alan Thompson <cloojure@gmail.com>
2016-02-07 17:04:01 -0800

    misc

--- push-docs.zsh
+++ push-docs.zsh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
 # Controlling values
-readonly ghPagesDir=~/gh-pages          # must be absolute pathname
+readonly ghPagesDir=~/gh-pages                  # must be absolute pathname
 readonly projName="tupelo"
 
-srcDir=$(pwd)/doc                       # absolute pathname of source
-destDir=${ghPagesDir}/${projName}       # absolute pathname of destination
+srcDir=$(pwd)/target/base+system+user+dev/doc   # absolute pathname of source
+destDir=${ghPagesDir}/${projName}               # absolute pathname of destination
 
 # Copy files
 mkdir --parents $destDir
