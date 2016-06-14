f7ea496d03dcafa5da716e37c03a0edae8558b31
Alan Thompson <cloojure@gmail.com>
2016-06-14 15:10:02 -0700

    misc

--- project.clj
+++ project.clj
@@ -4,9 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [ [org.clojure/clojure              "1.9.0-alpha6"]
-                  [org.clojure/core.incubator       "0.1.4"]
                   [org.clojure/core.async           "0.2.382"]
-                  [org.clojure/test.check           "0.9.0"]
                   [org.clojure/core.match           "0.3.0-alpha4"]
                   [org.clojure/math.combinatorics   "0.1.3"]
                   [org.clojure/tools.analyzer       "0.6.9"]
@@ -15,19 +13,19 @@
                   [cheshire                         "5.6.1"]
                   [prismatic/schema                 "1.1.2"]
                 ]
+  :profiles { :dev      {:dependencies [[org.clojure/test.check "0.9.0"]] }
+              :uberjar  {:aot :all}}
+  :global-vars { *warn-on-reflection* false }
+
   :plugins  [ [lein-codox "0.9.4"] ]
   :codox {:src-dir-uri "http://github.com/cloojure/tupelo/blob/master/"
           :src-linenum-anchor-prefix "L"}
   :deploy-repositories {  "snapshots" :clojars
                           "releases"  :clojars }
-  :update :always; :daily  
+  :update :daily  ; :always  
   :main ^:skip-aot tupelo.core
-  :target-path "target/%s"
-  :clean-targets [ "target" ]
-  :profiles { ; :dev      { :certificates ["clojars.pom"] }
-              :uberjar  { :aot :all }
-            }
-  :global-vars { *warn-on-reflection* false }
+  :target-path      "target/%s"
+  :clean-targets  [ "target" ]
 
   ; "lein test"         will not  run tests marked with the ":slow" metadata
   ; "lein test :slow"   will only run tests marked with the ":slow" metadata
@@ -36,5 +34,5 @@
                     :slow       :slow }
 
   :jvm-opts ^:replace ["-Xms1g" "-Xmx4g" ]
-; :jvm-opts ^:replace ["-Xms4g" "-Xmx8g" "-server"]
+; :jvm-opts ^:replace ["-Xms1g" "-Xmx8g" "-server"]
 )
