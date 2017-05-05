ec1a3eb32cbabcfce1f4375a7196b5dfa0f752a4
Alan Thompson <cloojure@gmail.com>
2017-05-04 17:00:21 -0700

    misc

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [ 
    ;[org.clojure/clojure              "1.9.0-alpha16"]
-    [org.clojure/clojure              "1.8.0"]
     [cheshire                         "5.7.0"]
     [clj-time                         "0.13.0"]
     [clojure-csv/clojure-csv          "2.0.2"]
     [danlentz/clj-uuid                "0.1.7"]
+    [org.clojure/clojure              "1.8.0"]
     [org.clojure/core.async           "0.2.395"]
     [org.clojure/core.match           "0.3.0-alpha4"]
     [org.clojure/data.xml             "0.0.8"]
@@ -17,8 +17,7 @@
     [org.clojure/tools.analyzer       "0.6.9"]
     [potemkin                         "0.4.3"]
     [prismatic/schema                 "1.1.3"]
-  ; [expectations                     "2.1.8"]
-                ]
+  ]
   :profiles { :dev      {:dependencies [[org.clojure/test.check "0.9.0"]] }
               :uberjar  {:aot :all}}
   :global-vars { *warn-on-reflection* false }
@@ -40,6 +39,5 @@
   :test-selectors { :default    (complement :slow)
                     :slow       :slow }
 
-  :jvm-opts ^:replace ["-Xms1g" "-Xmx4g" ]
-; :jvm-opts ^:replace ["-Xms1g" "-Xmx8g" "-server"]
+  :jvm-opts ^:replace ["-Xms1g" "-Xmx4g" ] ; "-server"
 )
