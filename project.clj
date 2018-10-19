c02e7a94ee54fffb23c294883463e43c683cfa61
Alan Thompson <cloojure@gmail.com>
2018-10-19 12:34:00 -0700

    misc

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tupelo "0.9.105"
+(defproject tupelo "0.9.106"
   :description "Tupelo:  Clojure With A Spoonful of Honey"
   :url "http://github.com/cloojure/tupelo"
   :license {:name "Eclipse Public License"
