7cf3a628007c2650d4ea4fc868010be241e2cd29
Alan Thompson <cloojure@gmail.com>
2019-06-05 17:20:42 -0700

    misc

--- chrome-headless.bash
+++ chrome-headless.bash
@@ -1,5 +1,31 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo "********** headless chrome - enter **********"
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome   --headless  --disable-gpu  --repl "$@"
+
+function isMac() {
+  if [[ $(uname -a) =~ "Darwin" ]]; then
+    true
+  else
+    false
+  fi
+}
+function isLinux() {
+  if [[ $(uname -a) =~ "Linux" ]]; then
+    true
+  else
+    false
+  fi
+}
+
+if isMac ; then
+  echo "found Mac"
+  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome   --headless  --disable-gpu  --repl "$@"
+fi
+
+if isLinux ; then
+  echo "found Linux"
+# google-chrome   --headless  --disable-gpu  --repl "$@"
+  google-chrome               --disable-gpu  --repl "$@"  
+fi
+
 echo "********** headless chrome - leave **********"
 
--- misc/chrome-headless.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "********** headless chrome - enter **********"
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome   --headless  --disable-gpu  --repl "$@"
-echo "********** headless chrome - leave **********"
-
