605bd854dedb1fda9e1a1af9e9bea4b6e08361f6
Alan Thompson <cloojure@gmail.com>
2019-12-12 20:42:24 -0800

    misc

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tupelo "0.9.177"
+(defproject tupelo "0.9.178"
   :description "Tupelo:  Clojure With A Spoonful of Honey"
   :url "https://github.com/cloojure/tupelo"
   :scm "https://github.com/cloojure/tupelo"
