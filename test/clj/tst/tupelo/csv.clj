3b3bf5bfdb9d00818587d120b85aaf6e876f7b0b
Alan Thompson <cloojure@gmail.com>
2020-02-26 12:53:59 -0800

    misc

--- test/clj/tst/tupelo/csv.clj
+++ test/clj/tst/tupelo/csv.clj
@@ -4,105 +4,102 @@
 ;   file epl-v10.html at the root of this distribution.  By using this software in any
 ;   fashion, you are agreeing to be bound by the terms of this license.
 ;   You must not remove this notice, or any other, from this software.
-(ns tst.tupelo.csv
+(ns       ; ^:test-refresh/focus
+  tst.tupelo.csv
   (:use tupelo.csv tupelo.core tupelo.test)
+  (:require [tupelo.parse :as parse])
   (:import  [java.io StringReader] ) )
 
-(def test1-str-no-label
+(def data1-str-no-label
   "01002,00006,4
   01002,00277,5
   01003,00277,5
   01008,01217,5
   01009,00439,5
-  01020,01193,5" )
+  01020,01193,5")
 
-(def test1-str-label
-  (str "zip_postal_code,store$num,chain#rank" \newline test1-str-no-label) )
+(def data1-str
+  (str "zip_postal_code,store$num,chain#rank"
+    \newline
+    data1-str-no-label))
 
-(def test2-str-label
+(def data2-str-label
   "ZIP_POSTAL_CODE|STORE_NUM|CHAIN_RANK
   01002|00006|4
   01002|00277|5
   01003|00277|5
   01008|01217|5
   01009|00439|5
-  01020|01193|5" )
+  01020|01193|5")
 
-(def test1-expected
-  [ { :zip-postal-code "01002" :store-num "00006" :chain-rank "4" }
-    { :zip-postal-code "01002" :store-num "00277" :chain-rank "5" }
-    { :zip-postal-code "01003" :store-num "00277" :chain-rank "5" }
-    { :zip-postal-code "01008" :store-num "01217" :chain-rank "5" }
-    { :zip-postal-code "01009" :store-num "00439" :chain-rank "5" }
-    { :zip-postal-code "01020" :store-num "01193" :chain-rank "5" } ] )
+(def expected-1-entity
+  [{:zip-postal-code "01002" :store-num "00006" :chain-rank "4"}
+   {:zip-postal-code "01002" :store-num "00277" :chain-rank "5"}
+   {:zip-postal-code "01003" :store-num "00277" :chain-rank "5"}
+   {:zip-postal-code "01008" :store-num "01217" :chain-rank "5"}
+   {:zip-postal-code "01009" :store-num "00439" :chain-rank "5"}
+   {:zip-postal-code "01020" :store-num "01193" :chain-rank "5"}])
 
-(def test2-expected
-  [ { :zipcode "01002" :store-id    6 }
-    { :zipcode "01002" :store-id  277 }
-    { :zipcode "01003" :store-id  277 }
-    { :zipcode "01008" :store-id 1217 }
-    { :zipcode "01009" :store-id  439 }
-    { :zipcode "01020" :store-id 1193 } ] )
+(def expected-1-attr
+  {:zip-postal-code ["01002" "01002" "01003" "01008" "01009" "01020"]
+   :store-num       ["00006" "00277" "00277" "01217" "00439" "01193"]
+   :chain-rank      ["4" "5" "5" "5" "5" "5"]})
 
-(def test3-expected
-  { :zip-postal-code    ["01002" "01002" "01003" "01008" "01009" "01020"]
-    :store-num          ["00006" "00277" "00277" "01217" "00439" "01193"]
-    :chain-rank         [    "4"     "5"     "5"     "5"     "5"     "5"] } )
+(def expected-2-entity
+  [{:zipcode "01002" :store-id 6}
+   {:zipcode "01002" :store-id 277}
+   {:zipcode "01003" :store-id 277}
+   {:zipcode "01008" :store-id 1217}
+   {:zipcode "01009" :store-id 439}
+   {:zipcode "01020" :store-id 1193}])
 
-(def test4-expected
-  { :zipcode            ["01002" "01002" "01003" "01008" "01009" "01020"]
-    :store-id           [     6     277     277    1217     439    1193 ] } )
+(def expected-2-attr
+  {:zipcode  ["01002" "01002" "01003" "01008" "01009" "01020"]
+   :store-id [6 277 277 1217 439 1193]})
 
 (dotest
   ; basic parse-csv->rows test, using String
-  (let [result (parse->entities test1-str-label)]
-    (is (= result test1-expected)))
-
-  ; read PSV file instead of default CS"
-  (let [raw-maps (parse->entities test2-str-label :delimiter \|)
-        result   (map #(hash-map :store-id (Long/parseLong (:STORE-NUM %))
-                         :zipcode (:ZIP-POSTAL-CODE %))
-                   raw-maps)]
-    (is (= result test2-expected)))
+  (let [result (parse->entities data1-str)]
+    (is= result expected-1-entity))
 
   ; no header row in file, user spec :labels
-  (let [result (parse->entities test1-str-no-label
+  (let [result (parse->entities data1-str-no-label
                  :labels [:zip-postal-code :store-num :chain-rank])]
-    (is (= result test1-expected))))
+    (is= result expected-1-entity))
 
-  (dotest
-    ; basic parse-csv->rows test, using Reader
-    (let [result (parse->entities (StringReader. test1-str-label))]
-      (is (= result test1-expected))) )
+  ; basic parse-csv->rows test, using Reader
+  (let [result (parse->entities (StringReader. data1-str))]
+    (is= result expected-1-entity))
+
+  ; read PSV file instead of default CSV"
+  (let [result (forv [entity (parse->entities data2-str-label :delimiter \|)]
+                 {:store-id (parse/parse-long (grab :STORE-NUM entity))
+                  :zipcode  (grab :ZIP-POSTAL-CODE entity)})]
+    (is= result expected-2-entity)))
 
 (dotest
   (is= {} (entities->attrs []))
   (is= [] (attrs->entities {}))
-  (let [result (entities->attrs test1-expected)]
-    (is (= result test3-expected)))
-  (let [result (entities->attrs test2-expected)]
-    (is (= result test4-expected))))
+
+  (is= (entities->attrs expected-1-entity) expected-1-attr)
+  (is= (entities->attrs expected-2-entity) expected-2-attr)
+
+  (is= (attrs->entities expected-1-attr) expected-1-entity)
+  (is= (attrs->entities expected-2-attr) expected-2-entity))
 
 (dotest
-  (let [result (attrs->entities test3-expected)]
-    (is (= result test1-expected)))
-  (let [result (attrs->entities test4-expected)]
-    (is (= result test2-expected))))
+  (is= (parse->attrs data1-str) expected-1-attr)
 
-(dotest
-  (let [result (parse->attrs test1-str-label)]
-    (is (= result test3-expected)))
-
-  (let [raw-maps (parse->attrs test2-str-label :delimiter \|)
-        result   {:store-id (map #(Long/parseLong %) (:STORE-NUM raw-maps))
+  (let [raw-maps (parse->attrs data2-str-label :delimiter \|)
+        result   {:store-id (mapv parse/parse-long (grab :STORE-NUM raw-maps))
                   :zipcode  (:ZIP-POSTAL-CODE raw-maps)}]
-    (is (= result test4-expected))))
+    (is= result expected-2-attr)))
 
 (dotest
   (let [test2-str-label-error "zipcode, store_num, chain_rank
                                   01002,00006,4
                                   01002,00277,5
-                                  01003,00277" ]
+                                  01003,00277"]
     (throws? (parse->entities test2-str-label-error))))
 
 
