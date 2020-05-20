685150f65d8747339886143ff43e19c193dbe3de
Alan Thompson <cloojure@gmail.com>
2020-05-20 09:12:06 -0700

    v0.9.204a

--- src/clj/tupelo/math.clj
+++ src/clj/tupelo/math.clj
@@ -3,25 +3,21 @@
     [tupelo.core :as t]
     [schema.core :as s]))
 
-#?(:clj
-   (do
+(def ^:dynamic UNIT
+  "The unit magnitued & type used for round, floor, ceil, etc"
+  (double 1))
 
-     (def ^:dynamic UNIT
-       "The unit magnitued & type used for round, floor, ceil, etc"
-       (double 1))
+(defmacro with-units
+  "Re-define the default UNIT to the specified magnitude & type for the enclosed operations"
+  [u & forms]
+  `(binding [UNIT u]
+     ~@forms))
 
-     (defmacro with-units
-       "Re-define the default UNIT to the specified magnitude & type for the enclosed operations"
-       [u & forms]
-       `(binding [UNIT u]
-          ~@forms))
+#_(s/defn unit-type []
+    (cond =)
+    )
 
-     #_(s/defn unit-type []
-         (cond =)
-         )
+(s/defn floor :- s/Num
+  [x :- s/Num]
+ )
 
-     (s/defn floor :- s/Num
-       [x :- s/Num]
-       )
-
-     ))
