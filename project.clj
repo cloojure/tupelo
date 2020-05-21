7858838b6f81a8971ec56fb74cf5950e583fd9ff
Alan Thompson <cloojure@gmail.com>
2020-05-21 01:06:18 -0700

    v0.9.206

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tupelo "0.9.205"
+(defproject tupelo "0.9.206"
   :description "Tupelo:  Clojure With A Spoonful of Honey"
   :url "https://github.com/cloojure/tupelo"
   :scm "https://github.com/cloojure/tupelo"
