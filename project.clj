56dae2fc008eae32e3e4b29647929ab85e6ef51c
Alan Thompson <cloojure@gmail.com>
2021-03-25 16:58:41 -0700

    v21.03.25

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tupelo "21.01.26b"
+(defproject tupelo "21.03.25"
   :description "Tupelo:  Clojure With A Spoonful of Honey"
   :url "https://github.com/cloojure/tupelo"
   :scm "https://github.com/cloojure/tupelo"
