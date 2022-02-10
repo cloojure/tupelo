ece74d3adea664e83712d3cb4e0a9b72c03dfb3d
Alan <cloojure@gmail.com>
2022-02-10 06:27:51 -0800

    v22.02.09

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tupelo "22.02.03c"
+(defproject tupelo "22.02.09"
   :description "Tupelo:  Clojure With A Spoonful of Honey"
   :url "https://github.com/cloojure/tupelo"
   :scm "https://github.com/cloojure/tupelo"
