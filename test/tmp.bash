61a22ffcbaa156110bb688551f170b188beb0548
Alan Thompson <cloojure@gmail.com>
2023-03-23 15:14:19 -0700

    Migrated all test files to a single root (killed test/clj, test/cljc)

---
+++ test/tmp.bash
@@ -0,0 +1,10 @@
+#!/bin/bash 
+set -v
+
+git mv clj/tst/tupelo/cuid                  tst/tupelo
+git mv clj/tst/tupelo/parse                 tst/tupelo
+git mv clj/tst/tupelo/math                  tst/tupelo
+git mv clj/tst/tupelo/dev                   tst/tupelo
+git mv clj/tst/tupelo/java_time             tst/tupelo
+git mv clj/tst/tupelo/x                     tst/tupelo
+
