a524bfbfebb05fbe76d439e36997d574b86a6c61
Alan Thompson <cloojure@gmail.com>
2023-03-30 10:56:41 -0700

    misc

--- src/tupelo/uuid.clj
+++ src/tupelo/uuid.clj
@@ -1,15 +1,17 @@
 (ns tupelo.uuid
-  (:use tupelo.core)
   (:refer-clojure :exclude [rand])
   (:require
     [clj-uuid :as uuid]
     [clojure.core :exclude [rand]]
     [schema.core :as s]
+    [tupelo.core :as t]
     [tupelo.schema :as tsk]
     [tupelo.string :as str]
     [tupelo.core.impl :as impl])
   (:import
     [java.util UUID]))
+
+; #TODO add code & tests for cljs
 
 (def const-null-str
   "A null UUID string '00000000-0000-0000-0000-000000000000' "
@@ -19,21 +21,6 @@
   "A dummy UUID string '00000000-0000-0000-0000-000000000000' "
   "cafebabe-1953-0510-0970-0123456789ff")
 
-; #todo add code & tests for cljs
-
-(def uuid-str? ; #todo add tests for cljc
-  "Returns true iff the string shows a valid UUID-like pattern of hex digits. Does not
-  distinguish between UUID subtypes."
-  impl/uuid-str?)
-
-(def null-str
-  "Returns a null UUID string '00000000-0000-0000-0000-000000000000' "
-  (const->fn const-null-str))
-
-(def dummy-str
-  "Returns a dummy UUID string 'cafebabe-1953-0510-0970-0123456789ff' "
-  (const->fn const-dummy-str))
-
 (def const-null-obj
   "A null UUID object '00000000-0000-0000-0000-000000000000' "
   (UUID/fromString const-null-str))
@@ -42,13 +29,27 @@
   "A dummy UUID object '00000000-0000-0000-0000-000000000000' "
   (UUID/fromString const-dummy-str))
 
+;-----------------------------------------------------------------------------
+(def uuid-str? ; #todo add tests for cljc
+  "Returns true iff the string shows a valid UUID-like pattern of hex digits. Does not
+  distinguish between UUID subtypes."
+  impl/uuid-str?)
+
+(def null-str
+  "Returns a null UUID string '00000000-0000-0000-0000-000000000000' "
+  (t/const->fn const-null-str))
+
+(def dummy-str
+  "Returns a dummy UUID string 'cafebabe-1953-0510-0970-0123456789ff' "
+  (t/const->fn const-dummy-str))
+
 (def null
   "Returns a null UUID object '00000000-0000-0000-0000-000000000000' "
-  (const->fn const-null-obj))
+  (t/const->fn const-null-obj))
 
 (def dummy
   "Returns a dummy UUID object 'cafebabe-1953-0510-0970-0123456789ff'"
-  (const->fn const-dummy-obj))
+  (t/const->fn const-dummy-obj))
 
 ;-----------------------------------------------------------------------------
 (s/defn rand :- UUID
@@ -65,7 +66,7 @@
 (counted-reset!) ; initialize upon load
 
 (defn counted-str []
-  (let [cnt      (swap-out! uuid-counter inc)
+  (let [cnt      (t/swap-out! uuid-counter inc)
         uuid-str (format "%08x-aaaa-bbbb-cccc-ddddeeeeffff" cnt)]
     uuid-str))
 
