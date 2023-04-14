e5833eb906b09c738ef77f82dd8b3b630ea13d58
Alan Thompson <cloojure@gmail.com>
2023-04-14 08:15:26 -0700

    misc

--- build.clj
+++ build.clj
@@ -2,23 +2,32 @@
   (:require
     [clojure.tools.build.api :as b]
     [deps-deploy.deps-deploy :as dd]
+    [tupelo.misc :as misc]
     ))
 
-(def version "23.03.14a") ; snapshot versions MUST look like `23.03.03-SNAPSHOT` (i.e. no letters like `-03a`)
-(def tag (str "v" version)) ; ***** ASSUMES YOU CREATE A GIT TAG LIKE `v23.01.31` *****
+(def version-str "23.03.14a") ; snapshot versions MUST look like `23.03.03-SNAPSHOT` (i.e. no letters like `-03a`)
+(def tag-str (str "v" version-str)) ; ***** ASSUMES YOU CREATE A GIT TAG LIKE `v23.01.31` *****
 (def lib-name 'tupelo/tupelo) ; must be a namespaced-qualified symbol, interpreted as `group-id/artifact-id`
 (def scm-root "github.com/cloojure/tupelo")
 
 (def build-folder "target")
 (def jar-content (str build-folder "/classes")) ; folder where we collect files to pack in a jar
 (def basis (b/create-basis {:project "deps.edn"})) ; basis structure (read details in the article)
-(def jar-file-name (format "%s/%s-%s.jar" build-folder (name lib-name) version)) ; path for result jar file
+(def jar-file-name (format "%s/%s-%s.jar" build-folder (name lib-name) version-str)) ; path for result jar file
 
 (defn clean-files
   "Delete all compiler output files (i.e. `.target/**/*`)"
   [& args]
   (b/delete {:path build-folder})
   (println (format "Build folder \"%s\" removed" build-folder)))
+
+(defn tag-release
+  "Tag release by prepending a `v` char to the version string and calling `git tag`
+    (eg version `23.03.15` => tag `v23.03.15`)."
+  []
+  (def  tag-str (str \v version-str))
+  (let [])
+  )
 
 (defn build-jar
   "Build a new, clean JAR file from source-code."
@@ -32,10 +41,10 @@
 
   (b/write-pom {:class-dir jar-content ; create pom.xml
                 :lib       lib-name
-                :version   version
+                :version   version-str
                 :basis     basis
                 :src-dirs  ["src"]
-                :scm       {:tag                 tag
+                :scm       {:tag                 tag-str
                             :url                 (str "https://" scm-root)
                             :connection          (str "scm:git:git://" scm-root ".git")
                             :developerConnection (str "scm:git:ssh://git@" scm-root ".git")}})
