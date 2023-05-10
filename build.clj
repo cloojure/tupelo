e3df04ec1543f50ab1683030b96ac49278558f3a
Alan Thompson <cloojure@gmail.com>
2023-05-10 11:59:46 -0700

    misc

--- build.clj
+++ build.clj
@@ -23,7 +23,7 @@
 (def git-tag-str (str "v" version-str)) ; a git tag like `v23.01.31` will be added
 (def jar-content (str build-folder "/classes")) ; folder where we collect files to pack in a jar
 (def basis (b/create-basis {:project "deps.edn"})) ; basis structure (read details in the article)
-(def jar-file-name (format "%s/%s-%s.jar" build-folder (name lib-name) version-str)) ; path for result jar file
+(def jar-file-name (format "%s/%s-%s.jar" build-folder (name lib-name) version-str)) ; eg `target/tupelo-23.05.04.jar`
 
 ;---------------------------------------------------------------------------------------------------
 ; code
