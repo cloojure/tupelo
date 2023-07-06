c2b57ecd536118ac1a9f930170c7cbdf274fbbb5
Alan Thompson <cloojure@gmail.com>
2023-07-05 17:11:37 -0700

    misc

--- deploy-clojars.bash
+++ deploy-clojars.bash
@@ -4,5 +4,5 @@ set -v
 env  \
   CLOJARS_USERNAME=cloojure  \
   CLOJARS_PASSWORD=`cat ~/alan/clojars.txt`  \
-  clj -T:build tupelo.deps-build-deploy/deploy-clojars
+  clj  -T:build  tupelo.deps-build-deploy/deploy-clojars
 
