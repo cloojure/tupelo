7a5c8e02bef1f107e44089ec2655955e2c144ee4
Alan Thompson <thompson2526@gmail.com>
2024-12-16 10:15:15 -0800

    misc

--- test/tst/tupelo/demo.clj
+++ test/tst/tupelo/demo.clj
@@ -10,15 +10,15 @@
     [clojure.string :as str]
     [clojure.string :as str]
     [schema.core :as s]
-    [tupelo.core :as t] )
-  (:import java.time.Instant) )
+    [tupelo.core :as t])
+  (:import java.time.Instant))
 
 ; #todo add generative testing?
 ; #todo add clojure.spec testing?
 
 (verify
   (is (.isAfter (Instant/now) ; create clojure.java.time ns
-        (Instant/parse "2017-12-31T13:14:15z")))
+                (Instant/parse "2017-12-31T13:14:15z")))
   (is (= 5 (+ 2 3))))
 
 ; (x ...)     =>  "function call on x" (parens = "function call")
@@ -43,7 +43,7 @@
     [1 2 [(quote quote) 3]]
     [1 2 [(symbol "quote") 3]]
     [1 2 '(quote 3)]
-    [1 2 ''3] ))
+    [1 2 ''3]))
 
 (verify             ; empty lists/vectors
   (is= ()           ; should be error! parens => function call (here with missing function)
@@ -53,38 +53,38 @@
 
 
 (verify
-  (is= nil        (seq []))
+  (is= nil (seq []))
 
-  (is= []         (take 1 nil))
-  (is= []         (take 1 []))
-  (is= []         (take 2 []))
+  (is= [] (take 1 nil))
+  (is= [] (take 1 []))
+  (is= [] (take 2 []))
 
-  (is= []         (take 0 [1 2]))
-  (is= [1]        (take 1 [1 2]))
-  (is= [1 2]      (take 2 [1 2]))
-  (is= [1 2]      (take 3 [1 2]))
+  (is= [] (take 0 [1 2]))
+  (is= [1] (take 1 [1 2]))
+  (is= [1 2] (take 2 [1 2]))
+  (is= [1 2] (take 3 [1 2]))
 
-  (is= [        ] (take 0 "abc"))
-  (is= [\a      ] (take 1 "abc"))
-  (is= [\a \b   ] (take 2 "abc"))
+  (is= [] (take 0 "abc"))
+  (is= [\a] (take 1 "abc"))
+  (is= [\a \b] (take 2 "abc"))
   (is= [\a \b \c] (take 3 "abc"))
   (is= [\a \b \c] (take 4 "abc")))
 
 (verify
-  (is= []         (drop 1 nil))
-  (is= []         (drop 1 []))
-  (is= []         (drop 2 []))
+  (is= [] (drop 1 nil))
+  (is= [] (drop 1 []))
+  (is= [] (drop 2 []))
 
-  (is= [1 2]      (drop 0 [1 2]))
-  (is= [  2]      (drop 1 [1 2]))
-  (is= []         (drop 2 [1 2]))
-  (is= []         (drop 3 [1 2]))
+  (is= [1 2] (drop 0 [1 2]))
+  (is= [2] (drop 1 [1 2]))
+  (is= [] (drop 2 [1 2]))
+  (is= [] (drop 3 [1 2]))
 
   (is= [\a \b \c] (drop 0 "abc"))
-  (is= [   \b \c] (drop 1 "abc"))
-  (is= [      \c] (drop 2 "abc"))
-  (is= [        ] (drop 3 "abc"))
-  (is= [        ] (drop 4 "abc")))
+  (is= [\b \c] (drop 1 "abc"))
+  (is= [\c] (drop 2 "abc"))
+  (is= [] (drop 3 "abc"))
+  (is= [] (drop 4 "abc")))
 
 (verify
   (is= [\a \b \c] (seq "abc"))
@@ -92,31 +92,31 @@
 
   (is= "abc" (str \a \b \c))
   (is= "abc" (str "ab" "c"))
-  (is= "abc" (str "ab" \c ))
+  (is= "abc" (str "ab" \c))
 
-  (is= "abc" (str/join [\a \b \c] ))
-  (is= "abc" (str/join ["ab" "c"] ))
-  (is= "abc" (str/join ["ab" \c ] ))
-)
+  (is= "abc" (str/join [\a \b \c]))
+  (is= "abc" (str/join ["ab" "c"]))
+  (is= "abc" (str/join ["ab" \c]))
+  )
 
 (verify
-  (is   (sequential?  []     ))
-  (is   (sequential?  [1 2 3]))
-  (is   (sequential? '(1 2 3)))
-  (isnt (sequential?  42     ))
-  (isnt (sequential?  :x     ))
-  (isnt (sequential?  "abc"  ))
-  (isnt (sequential?  {:a 1} ))
-  (isnt (sequential? #{:a 1} )) )
+  (is (sequential? []))
+  (is (sequential? [1 2 3]))
+  (is (sequential? '(1 2 3)))
+  (isnt (sequential? 42))
+  (isnt (sequential? :x))
+  (isnt (sequential? "abc"))
+  (isnt (sequential? {:a 1}))
+  (isnt (sequential? #{:a 1})))
 
 ; As of Clojure 1.9.0, seqable? is native to clojure
 (verify
-  (is   (seqable? "abc"))
-  (is   (seqable? {1 2 3 4}))
-  (is   (seqable? #{1 2 3}))
-  (is   (seqable? '(1 2 3)))
-  (is   (seqable? [1 2 3]))
-  (is   (seqable? (byte-array [1 2])))
+  (is (seqable? "abc"))
+  (is (seqable? {1 2 3 4}))
+  (is (seqable? #{1 2 3}))
+  (is (seqable? '(1 2 3)))
+  (is (seqable? [1 2 3]))
+  (is (seqable? (byte-array [1 2])))
   (isnt (seqable? 1))
   (isnt (seqable? \a)))
 
@@ -132,5 +132,5 @@
 (verify
   (is= #{} (empty #{1 2 3}))
   (is= [] (empty [1 2 3]))
-  (is= {} (empty {:a 1 :b 2})) )
+  (is= {} (empty {:a 1 :b 2})))
 
